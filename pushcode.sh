f78db074a9aac51abc7cd2bd109355ae509f61dc
Jiang Wu <john.x.wu@ericsson.com>
2021-10-22 12:35:44 +0300

    update code in Fri Oct 22 12:35:44 EEST 2021: test

--- pushcode.sh
+++ pushcode.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+publish_to_blog="${1:-"n"}"
+
 def_msg=""
-additional_msg="${1:-$def_msg}"
+additional_msg="${2:-$def_msg}"
+
+if [ "$publish_to_blog" == "y" ];
+then
+  hexo g -d
+elif [ "$publish_to_blog" == "n" ];
+then
+  hexo g
+else
+  echo "Unrecognized command for hexo deploy!"
+  exit 1
+fi
 
 git add .
 git commit -m "update code in $(date): ${additional_msg}"
