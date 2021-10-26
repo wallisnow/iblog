a5d2257a13c57f9d57f508abf0e90048e98d8f27
Jiang Wu <john.x.wu@ericsson.com>
2021-10-26 09:36:39 +0300

    update code in Tue Oct 26 09:36:39 EEST 2021: n

--- pushcode.sh
+++ pushcode.sh
@@ -5,11 +5,9 @@ publish_to_blog="${2:-"n"}"
 def_msg=""
 additional_msg="${1:-$def_msg}"
 
-if [ "$publish_to_blog" == "y" ];
-then
+if [ "$publish_to_blog" == "y" ]; then
   hexo g -d
-elif [ "$publish_to_blog" == "n" ];
-then
+elif [ "$publish_to_blog" == "n" ]; then
   hexo g
 else
   echo "Unrecognized command for hexo deploy!, only 'y' or 'n' should be applied"
